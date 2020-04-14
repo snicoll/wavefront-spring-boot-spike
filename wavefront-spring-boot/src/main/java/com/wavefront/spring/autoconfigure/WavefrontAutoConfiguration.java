35436e09bf0cb6d367bee7e52e7cc0f816ee7de4
Stephane Nicoll <snicoll@pivotal.io>
2020-04-14 17:15:29 +0200

    Restructure auto-configuration to split metrics

--- wavefront-spring-boot/src/main/java/com/wavefront/spring/autoconfigure/WavefrontAutoConfiguration.java
+++ wavefront-spring-boot/src/main/java/com/wavefront/spring/autoconfigure/WavefrontAutoConfiguration.java
@@ -16,32 +16,29 @@
 
 package com.wavefront.spring.autoconfigure;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.wavefront.sdk.common.application.ApplicationTags;
-import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Tags;
-import io.micrometer.wavefront.WavefrontMeterRegistry;
 
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
- * Advanced Auto-configuration for Wavefront.
+ * {@link EnableAutoConfiguration Auto-configuration} to integrate with Wavefront metrics
+ * and tracing.
  *
  * @author Stephane Nicoll
  */
 @Configuration
-@ConditionalOnClass({ WavefrontMeterRegistry.class, MeterRegistryCustomizer.class, ApplicationTags.class })
+@ConditionalOnClass(ApplicationTags.class)
 @EnableConfigurationProperties(WavefrontProperties.class)
+@Import(WavefrontMetricsConfiguration.class)
 public class WavefrontAutoConfiguration {
 
 	@Bean
@@ -50,26 +47,6 @@ public class WavefrontAutoConfiguration {
 			ObjectProvider<ApplicationTagsBuilderCustomizer> customizers) {
 		return new ApplicationTagsFactory(customizers.orderedStream().collect(Collectors.toList()))
 				.createFromProperties(properties);
-	}
-
-	@Bean
-	public MeterRegistryCustomizer<WavefrontMeterRegistry> wavefrontTagsMeterRegistryCustomizer(
-			ApplicationTags applicationTags) {
-		return (registry) -> registry.config().commonTags(createTagsFrom(applicationTags));
-	}
-
-	private Iterable<Tag> createTagsFrom(ApplicationTags applicationTags) {
-		Map<String, String> tags = new HashMap<>();
-		PropertyMapper mapper = PropertyMapper.get().alwaysApplyingWhenNonNull();
-		mapper.from(applicationTags::getApplication).to((application) -> tags.put("application", application));
-		mapper.from(applicationTags::getService).to((service) -> tags.put("service", service));
-		mapper.from(applicationTags::getCluster).to((cluster) -> tags.put("cluster", cluster));
-		mapper.from(applicationTags::getShard).to((shard) -> tags.put("shard", shard));
-		if (applicationTags.getCustomTags() != null) {
-			tags.putAll(applicationTags.getCustomTags());
-		}
-		return Tags.of(tags.entrySet().stream().map((entry) -> Tag.of(entry.getKey(), entry.getValue()))
-				.collect(Collectors.toList()));
 	}
 
 }

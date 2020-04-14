66b559d6a0fb86ec75505ce5f999397a02614910
Stephane Nicoll <snicoll@pivotal.io>
2020-04-14 14:25:07 +0200

    Polish sample

--- wavefront-spring-boot-sample/src/main/java/sample/HomeController.java
+++ wavefront-spring-boot-sample/src/main/java/sample/HomeController.java
@@ -14,16 +14,17 @@
  * limitations under the License.
  */
 
-package com.example;
+package sample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class WavefrontSampleApplication {
+@RestController
+public class HomeController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WavefrontSampleApplication.class, args);
+	@GetMapping("/")
+	public String home() {
+		return "Hello World!";
 	}
 
 }
